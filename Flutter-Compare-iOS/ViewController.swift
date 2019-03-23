4751bd18e8999494e41667ae69a8ab911c7de55c
Shashikant86 <shashikant.jagtap@icloud.com>
2019-03-23 15:35:10 +0000

    native iOS apps to compare against Flutter

---
+++ Flutter-Compare-iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Flutter-Compare-iOS
+//
+//  Created by Shashikant Jagtap on 23/03/2019.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var pressmeButton: UIButton!
+    @IBAction func pressMe(_ sender: Any) {
+        print("button pressed")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Flutter-Compare-iOSTests/Flutter_Compare_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Flutter_Compare_iOSTests.swift
+//  Flutter-Compare-iOSTests
+//
+//  Created by Shashikant Jagtap on 23/03/2019.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+@testable import Flutter_Compare_iOS
+
+class Flutter_Compare_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
